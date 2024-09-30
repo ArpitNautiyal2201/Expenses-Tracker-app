7b79b8cfcfe4185064fad2f99d4ce94cbe36690d
ArpitNautiyal2201 <arpit22012002@gmail.com>
2024-09-30 22:23:22 +0530

    2nd time changes in app

--- lib/screens/home/views/main_screen.dart
+++ lib/screens/home/views/main_screen.dart
@@ -11,44 +11,98 @@ class MainScreen extends StatelessWidget {
         child: Column(
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Stack(
-                  alignment: Alignment.center,
+                Row(
                   children: [
-                    Container(
-                      width: 50,
-                      height: 50,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.orangeAccent
-                      ),
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.orangeAccent
+                          ),
+                        ),
+                        const Icon(Icons.person_3_rounded,color: Colors.white,),
+                      ],
                     ),
-                    const Icon(Icons.person_3_rounded,color: Colors.white,),
+                    const SizedBox(width: 8,),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Welcome!",
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.grey
+                          ),  
+                        ),
+                        Text(
+                          "Arpit Nautiyal",
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            
+                          )
+                        )
+                      ],
+                    ),
                   ],
                 ),
-                const SizedBox(width: 8,),
-                const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Welcome!",
-                      style: TextStyle(
-                        fontSize: 13,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.grey
-                      ),  
-                    ),
-                    Text(
-                      "Arpit Nautiyal",
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                        
-                      )
-                    )
-                  ],
-                )
+                IconButton(onPressed: () {}, icon: const Icon(Icons.settings_rounded))
               ],
+            ),
+            const Padding(padding: EdgeInsets.symmetric(vertical: 5)),
+            // SizedBox(height: 18,),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width / 2,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  Color(0xFFFF8D6C),
+                  
+                  Color(0xFF00B2E7),
+                  Color(0xFFE064F7),
+                ],
+            transform: GradientRotation(21 / 3)
+            ),
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(15),
+                  bottom: Radius.circular(40)
+                ),
+                
+              ),
+              child: Column(
+                children: [
+                  const Text("Total Balance"),
+                  const Text("₹ 1000"),
+
+                  Row(
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            width: 30,
+                            height: 30,
+                            decoration: const BoxDecoration(
+                              color: Colors.blueAccent,
+                              shape: BoxShape.circle,
+                            ),
+                            child: const Center(
+                              child: Icon(Icons.arrow_downward_rounded,size: 20,),
+                              
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
