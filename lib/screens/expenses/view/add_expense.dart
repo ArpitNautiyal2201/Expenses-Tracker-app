66d82b188fbc4a9a143df533cc382f66089f1984
ArpitNautiyal2201 <arpit22012002@gmail.com>
2024-10-10 19:12:32 +0530

    8th time changes in application

--- lib/screens/expenses/view/add_expense.dart
+++ lib/screens/expenses/view/add_expense.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
 
-class AddExpense extends StatelessWidget {
+class AddExpense extends StatefulWidget {
   const AddExpense({super.key});
+
+  @override
+  State<AddExpense> createState() => _AddExpenseState();
+}
+
+class _AddExpenseState extends State<AddExpense> {
+  TextEditingController expenseController = TextEditingController();
+  TextEditingController categoryController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+  DateTime dTime = DateTime.now();
+
+  @override
+  void initState() {
+    dateController.text = DateFormat('d/M/y, EEE').format(DateTime.now());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,9 +41,13 @@ class AddExpense extends StatelessWidget {
                   fontWeight: FontWeight.w500,
                 ),
               ),
+              const SizedBox(
+                height: 35,
+              ),
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.7,
                 child: TextFormField(
+                  controller: expenseController,
                   decoration: InputDecoration(
                       filled: true,
                       fillColor: Colors.white,
@@ -36,24 +57,11 @@ class AddExpense extends StatelessWidget {
                       ),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(50),
-                          borderSide: BorderSide.none)),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  prefixIcon: const Icon(
-                    FontAwesomeIcons.list,
-                    size: 16.7,
-                  ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
-                      borderSide: BorderSide.none),
-                  hintText: "Category",
+                          borderSide: BorderSide.none),
+                      hintText: "Amount",
+                      hintStyle: const TextStyle(
+                        color: Colors.grey,
+                      )),
                 ),
               ),
               const SizedBox(
@@ -61,28 +69,147 @@ class AddExpense extends StatelessWidget {
               ),
               TextFormField(
                 readOnly: true,
-                onTap: () => {
-                  showDatePicker(
+                onTap: () {
+                  showDialog(
+                      context: context,
+                      builder: (ctx) {
+                        return AlertDialog(
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextFormField(
+                               // controller: dateController,
+                                decoration: InputDecoration(
+                                    filled: true,
+                                    fillColor: Colors.white,
+                                    prefixIcon: const Icon(
+                                      Icons.abc,
+                                      size: 16.7,
+                                    ),
+                                    border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(50),
+                                        borderSide: BorderSide.none),
+                                    hintText: "Name",
+                                    hintStyle: const TextStyle(
+                                      color: Colors.grey,
+                                    )),
+                              ),
+                              SizedBox(height: 15,),
+                              TextFormField(
+                               // controller: dateController,
+                                decoration: InputDecoration(
+                                    filled: true,
+                                    fillColor: Colors.white,
+                                    prefixIcon: const Icon(
+                                      Icons.color_lens,
+                                      size: 16.7,
+                                    ),
+                                    border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(50),
+                                        borderSide: BorderSide.none),
+                                    hintText: "Color",
+                                    hintStyle: const TextStyle(
+                                      color: Colors.grey,
+                                    )),
+                              ),
+                              SizedBox(height: 15,),
+                              TextFormField(
+                               // controller: dateController,
+                                decoration: InputDecoration(
+                                    filled: true,
+                                    fillColor: Colors.white,
+                                    prefixIcon: const Icon(
+                                      FontAwesomeIcons.icons,
+                                      size: 16.7,
+                                    ),
+                                    border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(50),
+                                        borderSide: BorderSide.none),
+                                    hintText: "Icon",
+                                    hintStyle: const TextStyle(
+                                      color: Colors.grey,
+                                    )),
+                              ),
+                            ],
+                          ),
+                        );
+                      });
+                },
+                controller: categoryController,
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: const Icon(
+                      FontAwesomeIcons.list,
+                      size: 16.7,
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.add_circle_outline_sharp),
+                      iconSize: 20,
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                        borderSide: BorderSide.none),
+                    hintText: "Category",
+                    hintStyle: const TextStyle(
+                      color: Colors.grey,
+                    )),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: dateController,
+                readOnly: true,
+                onTap: () async {
+                  DateTime? newDate = await showDatePicker(
                     context: context,
-                    initialDate: DateTime.now(), 
-                    firstDate: DateTime.now(), 
-                    lastDate: DateTime.now().add(const Duration(days: 365)),
-                  )
+                    initialDate: dTime,
+                    firstDate: DateTime.now().add(const Duration(days: -1000)),
+                    lastDate: DateTime.now().add(const Duration(days: 1000)),
+                  );
+                  if (newDate != null) {
+                    setState(() {
+                      dateController.text =
+                          DateFormat('d/M/y, EEE').format(newDate);
+                      dTime = newDate;
+                    });
+                  }
                 },
                 decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  prefixIcon: const Icon(
-                    FontAwesomeIcons.calendar,
-                    size: 16.7,
-                  ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
-                      borderSide: BorderSide.none),
-                  hintText: "Date",
-                ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: const Icon(
+                      FontAwesomeIcons.calendar,
+                      size: 16.7,
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                        borderSide: BorderSide.none),
+                    hintText: "Date",
+                    hintStyle: const TextStyle(
+                      color: Colors.grey,
+                    )),
               ),
-              TextButton(onPressed: () {}, child: const Text("Save Now"))
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: kToolbarHeight,
+                child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.blueAccent),
+                    child: const Text(
+                      "Save Now",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    )),
+              )
             ],
           ),
         ),
